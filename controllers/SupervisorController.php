e109aefd4008c617adc79b6bf88281ff032c6d91
1michael4 <Mdavidramado@gmail.com>
2020-10-25 13:40:22 -0600

    arreglos

---
+++ controllers/SupervisorController.php
@@ -0,0 +1,13 @@
+<?php
+
+class SupervisorController{
+
+    public function index(){
+        require_once('views/supervisor/index_supervisor.php');
+    }
+
+    
+}
+
+
+?>
---
+++ views/supervisor/index_supervisor.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/all/css/style.css">
+</head>
+<body>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" style="color: #000000;" href="#">SENA-LAB</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: #000000;" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: #000000;" href="#">Administradores</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color: #000000;" href="#">Instructores</a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" style="color: #000000;" href="#">Salir</a>
+            </li>
+          </ul>
+    </nav>
+
+    <head>
+        <div class="jumbotron">
+            <h1 class="display-4">Hola de nuevo, Supervisor!</h1>
+            <p class="lead">En esta parte podrás visualizar lo que hace el Administrador y también lo que hace el Usuario en este caso, los instructores en Servicio Nacional de Aprendizaje <b>SENA</b></p>
+            <hr class="my-4">
+            <p>Puedes entrar a este apartado cualquier día y cualquier hora! Solamente con tu Rol: Supervisor y Contraseña.</p>
+          </div>
+    </head>
+
+    <section>
+        <div class="col-4">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-warning" style="color: #000000;">Administrador</li>
+                <li class="list-group-item">Crear Usuarios</li>
+                <li class="list-group-item">Ver Usuarios</li>
+                <li class="list-group-item">Editar Usuarios</li>
+                <li class="list-group-item">Eliminar Usuarios</li>
+            </ul>
+        </div>
+
+        <div class="col-md-4" >
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-success" style="color: #000000;">Usuario</li>
+                <li class="list-group-item ">Editar Perfil</li>
+                <li class="list-group-item">Ver Certificado</li>
+                <li class="list-group-item">Adjuntar Incapacidad</li>
+                <li class="list-group-item">Descargar Certificado</li>
+            </ul>
+        </div>
+    </section>
+    
+    <footer class="footer">
+        <p>Todos los derechos reservados a Sena-lab</p>
+        <p class=" "> Copyright © 2020 </p>            
+    </footer>
+
+</body>
+</html>
