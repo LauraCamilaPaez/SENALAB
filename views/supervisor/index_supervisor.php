e6e77314c13f16b5b9b893ddc730c923831905e8
1michael4 <Mdavidramado@gmail.com>
2020-10-31 22:46:06 -0500

    cambios

--- views/supervisor/index_supervisor.php
+++ views/supervisor/index_supervisor.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/all/css/style.css">
 </head>
 <body>
@@ -29,6 +28,7 @@
           </ul>
     </nav>
 
+    
     <head>
         <div class="jumbotron">
             <h1 class="display-4">Hola de nuevo, Supervisor!</h1>
@@ -39,7 +39,7 @@
     </head>
 
     <section>
-        <div class="colsv col-md-4">
+        <div class="col-md-4">
             <ul class="list-group">
                 <li class="list-group-item list-group-item-warning" style="color: #000000;">Administrador</li>
                 <li class="list-group-item">Crear Usuarios</li>
@@ -48,8 +48,7 @@
                 <li class="list-group-item">Eliminar Usuarios</li>
             </ul>
         </div>
-
-        <div class="colsv col-md-4" >
+        <div class="col-md-4">
             <ul class="list-group">
                 <li class="list-group-item list-group-item-success" style="color: #000000;">Usuario</li>
                 <li class="list-group-item ">Editar Perfil</li>
@@ -60,10 +59,14 @@
         </div>
     </section>
     
-    <footer class="footer">
+    <footer class="svfooter">
         <p>Todos los derechos reservados a Sena-lab</p>
         <p class=" "> Copyright © 2020 </p>            
     </footer>
 
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
